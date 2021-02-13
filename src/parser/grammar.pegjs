d34b34b473493ff3bbf9934aa3d7e0a7eb340924
Oliver Ross <Oliver.Ross@pamediagroup.com>
2021-02-13 14:06:57 +0000

    feat(parser): parse simple and query top level rules

--- src/parser/grammar.pegjs
+++ src/parser/grammar.pegjs
@@ -5,51 +5,38 @@ Mapping "mapping details and tree" = _ details:MappingDetails* _ tree:MappingTre
   }
 }
 
-MappingDetails "mapping details" = NVDMappingDetails
-
-NVDMappingDetails "full mapping details" = name:Char+ ':' version:Char+ ':' description:Char* {
+MappingDetails "mapping details" = _ head:Char+ _ tail:( _ '/' _ Char+)* '/'* {
+  const name = head.join('').trim()
+  const [ version, description ] = tail
   return {
-    name: name.join('').trim(),
-    version: version.join('').trim(),
-    description: description.join('').trim()
-  }
-} / NVMappingDetails
-
-NVMappingDetails "mapping details with no description" = name:Char+ ':' version:Char* {
-  return {
-    name: name.join('').trim(),
-    version: version.join('').trim()
-  }
-} / NMappingDetails
-
-NMappingDetails "mapping details with just a name" = name:Char+ {
-  return {
-    name: name.join('').trim()
+    ...(name ? { name }: {}),
+    ...(version ? { version: version.pop().join('').trim() }: {}),
+    ...(description ? { description: description.pop().join('').trim() }: {})
   }
 }
 
-MappingTree "a mapping tree" = '{' _ rules:MappingRule* _ '}' {
-  return rules
+MappingTree "a mapping tree" = '{' _ head:MappingRule* tail:(EOL MappingRule)* _ '}' {
+  return [ ...head, ...tail ]
 }
 
 MappingRule "a mapping rule" = _ rule:QueryMappingRule _ {
   return rule
 }
 
-QueryMappingRule "a mapping rule with a key and a query" = key:Char+ ':' query:Char+ EOL {
+QueryMappingRule "a mapping rule with a key and a query" = key:Char+ '/' query:Char+ {
   return {
     key: key.join('').trim(),
     query: query.join('').trim()
   }
 } / KeyMappingRule
 
-KeyMappingRule "a mapping rule with a key" = key:Char+ EOL {
+KeyMappingRule "a mapping rule with a key" = key:Char+ {
   return {
     key: key.join('').trim()
   }
 }
 
 EOL = [\r\n]
-Char = [^:{}\r\n]
+Char = [^/{}\r\n]
 _  = [ \t\r\n]*
 __ = [ \t\r\n]+
--- test/integration/parser/Parser.test.ts
+++ test/integration/parser/Parser.test.ts
@@ -6,21 +6,6 @@ beforeEach(() => {
   parser = new Parser()
 })
 
-/**
-
-TODO test this sort of thing
-
-name:version:description
-{
-  key
-  key:value
-  key
-}
-
-that with required should be enough for MVP - then work on nested mapping
-
- */
-
 describe('When I parse an empty mapping', () => {
   it('Then I get an empty tree', () => {
     expect(parser.parse('{}')).toEqual({ tree: [] })
@@ -28,15 +13,15 @@ describe('When I parse an empty mapping', () => {
     expect(parser.parse('\n\n{\n\n}\n\n')).toEqual({ tree: [] })
     expect(parser.parse('name\n{}')).toEqual({ name: 'name', tree: [] })
     expect(parser.parse('\n\nname\n\n{\n\n}\n\n')).toEqual({ name: 'name', tree: [] })
-    expect(parser.parse('name:version\n{}')).toEqual({ name: 'name', version: 'version', tree: [] })
-    expect(parser.parse('name:version:description\n{}')).toEqual({ name: 'name', version: 'version', description: 'description', tree: [] })
+    expect(parser.parse('name/version\n{}')).toEqual({ name: 'name', version: 'version', tree: [] })
+    expect(parser.parse('name/version/description\n{}')).toEqual({ name: 'name', version: 'version', description: 'description', tree: [] })
   })
 })
 
 describe('When I parse a mapping with a details line', () => {
   describe('And it is complete', () => {
     it('Then I get name, version and description in the result', () => {
-      const mapping = 'test-mapping:0.0.0:a test mapping for integration tests\t{}'
+      const mapping = 'test-mapping/0.0.0/a test mapping for integration tests\t{}'
 
       expect(parser.parse(mapping)).toEqual({
         name: 'test-mapping',
@@ -48,23 +33,11 @@ describe('When I parse a mapping with a details line', () => {
   })
   describe('And it has no description', () => {
     it('Then I get name and version in the result', () => {
-      const mapping = 'test-mapping:0.0.0\t{}'
+      const mapping = 'test-mapping/0.0.0\t{}'
 
       expect(parser.parse(mapping)).toEqual({
         name: 'test-mapping',
         version: '0.0.0',
-        tree: []
-      })
-    })
-  })
-  describe('And it has an empty description', () => {
-    it('Then I get name and version in the result and description is an empty string', () => {
-      const mapping = 'test-mapping:0.0.0:\t{}'
-
-      expect(parser.parse(mapping)).toEqual({
-        name: 'test-mapping',
-        version: '0.0.0',
-        description: '',
         tree: []
       })
     })
@@ -79,20 +52,9 @@ describe('When I parse a mapping with a details line', () => {
       })
     })
   })
-  describe('And it has an empty version and no description', () => {
-    it('Then I get name in the result and version is an empty string', () => {
-      const mapping = 'test-mapping:\n{}'
-
-      expect(parser.parse(mapping)).toEqual({
-        name: 'test-mapping',
-        version: '',
-        tree: []
-      })
-    })
-  })
   describe('And it is has whitespace (but not newlines) around delimeters', () => {
     it('Then the whitespace is trimmed', () => {
-      const mapping = 'test-mapping : 0.0.0 : a test mapping for integration tests\n{}'
+      const mapping = 'test-mapping / 0.0.0 / a test mapping for integration tests\n{}'
 
       expect(parser.parse(mapping)).toEqual({
         name: 'test-mapping',
@@ -104,23 +66,62 @@ describe('When I parse a mapping with a details line', () => {
   })
 })
 
-describe('When I parse a mapping that is invalid, I get an informative error', () => {
+describe('When I parse a mapping with a rule', () => {
+  describe('And the rule maps a top level property', () => {
+    it('Then the returned tree contains an element with the correct property key', () => {
+      const expected = {
+        tree: [{
+          key: 'keyname'
+        }]
+      }
+      expect(parser.parse('{keyname}')).toEqual(expected)
+      expect(parser.parse('\n\n{\n\n\t\tkeyname\n\t\n\t}\n')).toEqual(expected)
+    })
+  })
+  describe('And the rule maps a queried property', () => {
+    it('Then the returned tree contains the correct key and query', () => {
+      const queries = [
+        '$.store.book[*].author',
+        '$..author',
+        '$.store.*',
+        '$.store..price',
+        '$..book[2]',
+        '$..book[(@.length-1)]',
+        '$..book[-1:]',
+        '$..book[0,1]',
+        '$..book[:2]',
+        '$..book[?(@.isbn)]',
+        '$..book[?(@.price<10)]',
+        '$..book[?(@.price==8.95)]',
+        '$..book[?(@.price<30 && @.category=="fiction")]',
+        '$..*'
+      ]
+      queries.forEach(query => {
+        expect(parser.parse(`{
+          mappedKeyName / ${query}
+        }`)).toEqual({
+          tree: [{
+            key: 'mappedKeyName',
+            query
+          }]
+        })
+      })
+    })
+  })
+  // TODO multiple rules
+})
+
+describe('When I parse a mapping that is invalid, I get an error', () => {
   it('When the description line is weird', () => {
-    expect(() => parser.parse('test-mapping::description'))
-      .toThrowError('ParseError: Expected mapping details and tree but "t" found.')
-    expect(() => parser.parse(':version'))
-      .toThrowError('ParseError: Expected mapping details and tree but ":" found.')
-    expect(() => parser.parse(':version:'))
-      .toThrowError('ParseError: Expected mapping details and tree but ":" found.')
-    expect(() => parser.parse('::'))
-      .toThrowError('ParseError: Expected mapping details and tree but ":" found.')
-    expect(() => parser.parse(':'))
-      .toThrowError('ParseError: Expected mapping details and tree but ":" found.')
+    expect(() => parser.parse('test-mapping::description')).toThrow()
+    expect(() => parser.parse(':version')).toThrow()
+    expect(() => parser.parse(':version:')).toThrow()
+    expect(() => parser.parse('::')).toThrow()
+    expect(() => parser.parse(':')).toThrow()
   })
   it('When there is no mapping', () => {
-    expect(() => parser.parse(''))
-      .toThrowError('ParseError: Expected mapping details and tree but end of input found.')
-    expect(() => parser.parse('name:version:description'))
-      .toThrowError('ParseError: Expected mapping details and tree but "n" found.')
+    expect(() => parser.parse('')).toThrow()
+    expect(() => parser.parse('name:version:description')).toThrow()
   })
+  // TODO mapping rule invalid
 })
